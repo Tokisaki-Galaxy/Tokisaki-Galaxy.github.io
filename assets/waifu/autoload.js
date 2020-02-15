90b53ee12c9ec329430ad19d05f9208f7e1aecf7
Evil-Binary <xbinary200@gmail.com>
2020-02-15 12:16:52 +0800

    Sdf

--- assets/waifu/autoload.js
+++ assets/waifu/autoload.js
@@ -1,4 +1,7 @@
-const live2d_path = https://cdn.jsdelivr.net/gh/evil-binary/evil-binary.github.io/assets/waifu
+//This file has been abandoned
+
+const live2d_path = "https://cdn.jsdelivr.net/gh/evil-binary/evil-binary.github.io/assets/waifu"
+
 try {
     $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/evil-binary/evil-binary.github.io/assets/waifu/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
